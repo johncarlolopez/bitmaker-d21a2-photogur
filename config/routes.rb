799e3fbf863a1ee649c84c1cc0a2d1693daeb8a3
John Carlo Lopez <condottiere.jc@gmail.com>
2018-01-18 13:24:00 -0500

    Add User and it's create functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'pictures#index'
+
     resources :pictures
-    #
-    # get 'pictures' => 'pictures#index'
-    # get 'pictures/new' => 'pictures#new'
-    # get 'pictures/:id' => 'pictures#show' as 'picture'
-    # get 'pictures/:id/edit' => "pictures#edit"
-    #
-    # post 'pictures' => 'pictures#create'
-    #
-    # patch 'pictures/:id' => "pictures#update"
-    #
-    # delete 'pictures/:id' => 'pictures#destroy'
+
+    resources :users, only: [:new, :create]
 end
