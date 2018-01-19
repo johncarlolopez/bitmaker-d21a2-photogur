59372d68ab2a833b34346342603623f8ef52c3e9
John Carlo Lopez <condottiere.jc@gmail.com>
2018-01-19 14:33:09 -0500

    complete assignment: authorization

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,7 @@
 class PicturesController < ApplicationController
-  before_action :ensure_logged_in, except: [:show, :index]
+  before_action :ensure_logged_in, except: [:index, :show]
+  before_action :load_picture, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_user_owns_picture, only: [:edit, :update, :destroy]
 
   def index
     @most_recent_pictures = Picture.most_recent_five
@@ -7,7 +9,7 @@ class PicturesController < ApplicationController
     @years = Picture.years
   end
   def show
-    @picture = Picture.find(params[:id])
+    @user = @picture.user
   end
   def new
     @picture = Picture.new
@@ -18,7 +20,7 @@ class PicturesController < ApplicationController
     @picture.title = params[:picture][:title]
     @picture.artist = params[:picture][:artist]
     @picture.url = params[:picture][:url]
-
+    @picture.user = current_user
     # unless @picture.valid?
     #   # ap @picture.errors.messages
     #   @picture.errors.messages.each {|key,value|
@@ -27,7 +29,7 @@ class PicturesController < ApplicationController
     # end
     if @picture.save
       # if the picture gets saved, generate a get request to "/pictures" (the index)
-      redirect_to "/pictures"
+      redirect_to pictures_path
       # redirect_to
     else
       # otherwise render new.html.erb
@@ -35,12 +37,9 @@ class PicturesController < ApplicationController
     end
   end
   def edit
-    @picture = Picture.find(params[:id])
   end
 
   def update
-    @picture = Picture.find(params[:id])
-
     @picture.title = params[:picture][:title]
     @picture.artist = params[:picture][:artist]
     @picture.url = params[:picture][:url]
@@ -54,8 +53,22 @@ class PicturesController < ApplicationController
   end
 
   def destroy
-    @picture = Picture.find(params[:id])
     @picture.destroy
-    redirect_to "/pictures"
+    redirect_to pictures_url
+  end
+
+  private
+
+  def ensure_user_owns_picture
+    unless current_user == @picture.user
+      # ap @picture.user
+      # ap current_user
+      flash[:alert] = "You do not have rights to that picture"
+      redirect_to picture_path(params[:id])
+    end
+  end
+
+  def load_picture
+    @picture = Picture.find(params[:id])
   end
 end
