fce0d4566c8290171cdc89cb1881a5f2427707c4
John Carlo Lopez <condottiere.jc@gmail.com>
2018-01-19 14:41:28 -0500

    create code to prevent errors on first run

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,25 @@
 class UsersController < ApplicationController
 
   def new
+    #FIRST RUN
+    if User.count == 0
+      @user = User.new
+      @user.email = 'john@gmail.com'
+      @user.password = '1234'
+      @user.password_confirmation = '1234'
+
+      @user.save
+    end
     @user = User.new
+
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+        session[:user_id] = @user.id
+        redirect_to pictures_url, notice: "Logged in!"
+      else
       redirect_to root_url
     else
       render :new
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,19 @@ Picture.create(
   title:  'The old church on the coast of White sea',
   artist: 'Sergey Ershov',
   url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg'
+  user_id: 1
 )
 
 Picture.create(
   title:  'Sea Power',
   artist: 'Stephen Scullion',
   url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg'
+  user_id: 1
 )
 
 Picture.create(
   title:  'Into the Poppies',
   artist: 'John Wilhelm',
   url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg'
+  user_id: 1
 )
